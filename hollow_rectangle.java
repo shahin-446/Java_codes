4fcdb45f5a89a96d65bc74a00f46c26a886f5302
shahin-446 <84090319+shahin-446@users.noreply.github.com>
2022-12-23 14:29:47 +0530

    hollow rectangle pattern

---
+++ hollow_rectangle.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int row = sc.nextInt();
+        int col = sc.nextInt();
+        //outer loop
+        for(int i=1;i<=row;i++){
+            //inner loop
+            for(int j=1;j<=col;j++){
+                if(i==1 || i==row || j==1 || j==col){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
